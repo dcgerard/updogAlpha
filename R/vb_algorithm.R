80c8e2648920203c32abb74974ff98a9fe9ea5fa
David Gerard <gerard.1787@gmail.com>
2016-11-14 15:37:01 -0600

    initial commit

---
+++ R/vb_algorithm.R
@@ -0,0 +1,192 @@
+
+
+#' Using Parental DNA for Offspring Genotyping.
+#'
+#' This function takes parental and offspring sequence counts and returns
+#' posterior probabilities on the genotype of all offspring. This isn't fully
+#' Bayesian because I only update the parental probabilities with just their
+#' sequencing data.
+#'
+#' What is updog? Nothing much, what's up with you dawg!
+#'
+#' @param ocounts A vector of non-negative integers. The ith element is the
+#'   number of reads of the reference allele the ith child.
+#' @param osize A vector of positive integers. The ith element is the total
+#'   number of reads for the ith child.
+#' @param p1counts A vector of non-negative integers. The ith element is the
+#'   number of reads of the reference allele in the ith sample of parent 1.
+#' @param p1size A vector of positive integers. The ith element is the total
+#'   number of reads in the ith sample of parent 1.
+#' @param p2counts A vector of non-negative integers. The ith element is the
+#'   number of reads of the reference allele in the ith sample of parent 2.
+#' @param p2size A vector of positive integers. The ith element is the total
+#'   number of reads in the ith sample of parent 2.
+#' @param ploidy A positive integer. The number of copies of the genome in the
+#'   species.
+#'
+#' @author David Gerard
+#'
+#' @export
+#'
+updog <- function(ocounts, osize, p1counts, p1size, p2counts, p2size, ploidy) {
+
+  ## check input -------------------------------------------------------------
+  assertthat::assert_that(all(ocounts >= 0))
+  assertthat::assert_that(all(p1counts >= 0))
+  assertthat::assert_that(all(p2counts >= 0))
+  assertthat::assert_that(all(osize >= 1))
+  assertthat::assert_that(all(p1size >= 1))
+  assertthat::assert_that(all(p2size >= 1))
+  assertthat::assert_that(all(ocounts <= osize))
+  assertthat::assert_that(all(p1counts <= p1size))
+  assertthat::assert_that(all(p2counts <= p2size))
+  assertthat::assert_that(ploidy >= 1)
+
+  ## derive posteriors of parental genotypes given just parental sequence data.
+  r1vec <- bin_post(ncounts = p1counts, ssize = p1size, prior = ploidy)
+  r2vec <- bin_post(ncounts = p2counts, ssize = p2size, prior = ploidy)
+
+  ## derive offspring genotype probabilities given parental genotypes.
+  qarray <- get_q_array(ploidy = ploidy)
+
+
+  ## initialize parameters
+  phi_vec <- r1vec ## posterior prob of p1 genotype
+  psi_vec <- r2vec ## posterior prob of p2 genotype
+
+  harray <- sweep(qarray, MARGIN = 1, STATS = phi_vec, FUN = `*`)
+  harray <- sweep(harray, MARGIN = 2, STATS = psi_vec, FUN = `*`)
+  hl <- apply(harray, 3, sum)
+
+
+  pk <- seq(0, ploidy) / ploidy
+  lmat  <- outer(ocounts, log(pk), FUN = `*`)
+  olmat <- outer(osize - ocounts, log(1 - pk), FUN = `*`)
+
+  tmat <- lmat + olmat
+  tmat[ocounts == 0, 1] <- 0
+  tmat[ocounts == osize, ploidy + 1] <- 0
+  logprobmat <- sweep(tmat, MARGIN = 2, STATS = log(hl), FUN = `+`)
+  logprobmat[logprobmat == -Inf] <- NA
+  rowmax <- apply(logprobmat, 1, max, na.rm = TRUE)
+  logprobmat <- logprobmat - rowmax
+  logprobmat[is.na(logprobmat)] <- -Inf
+
+  exppmat <- exp(logprobmat)
+  postprob <- exppmat / rowSums(exppmat)
+
+  return(postprob)
+}
+
+#' Derive the probabilities of an offspring's genotype given its parental
+#' genotypes for all possible combinations of parental and offspring genotypes.
+#'
+#' @param ploidy A positive integer. The ploidy of the species.
+#'
+#' @author David Gerard
+#'
+#' @export
+#'
+get_q_array <- function(ploidy) {
+  assertthat::assert_that(ploidy > 0)
+  assertthat::are_equal(ploidy %% 2, 0)
+
+  qarray <- array(0, dim = rep(ploidy + 1, 3))
+
+  for(oindex in 0:ploidy) {
+    for (p1index in 0:ploidy) {
+      for (p2index in 0:ploidy) {
+        if (p1index + p2index < oindex) {
+          qarray[p1index + 1, p2index + 1, oindex + 1] <- 0
+        } else {
+          minval <- max(0, oindex - p2index)
+          maxval <- min(ploidy / 2, p1index)
+          aseq <- minval:maxval
+
+          p1prob <- stats::dhyper(x = aseq, m = p1index, n = ploidy - p1index, k = ploidy / 2)
+          p2prob <- stats::dhyper(x = oindex - aseq, m = p2index, n = ploidy - p2index, k = ploidy / 2)
+          qarray[p1index + 1, p2index + 1, oindex + 1] <- sum(p1prob * p2prob)
+        }
+      }
+    }
+  }
+
+  assertthat::assert_that(all(abs(apply(qarray, c(1, 2), sum) - 1) < 10 ^ -14))
+
+  return(qarray)
+}
+
+#' Calculates posterior probabilities of a genotype given just the sequence
+#' counts.
+#'
+#' The function assumes that all data come from independent samples on the same
+#' individual.
+#'
+#' @param ncounts A vector of non-negative integers. The ith element is the
+#'   number of counts of the ith sample.
+#' @param ssize A vector of positive integers. The ith element is the total
+#'   number of counts of the ith sample.
+#' @param prior A vector of non-negative numerics that sum to one. The prior
+#'   probability on the genotype. The first element is the prior probability of
+#'   zero reference alleles, the second element is the prior probability of one
+#'   reference allele, etc. The length of \code{prior} is one more than the
+#'   ploidy of the species. You can alternatively specify \code{prior} as the
+#'   ploidy of the individual, for which it will set a uniform prior on the
+#'   genotype. For example, setting \code{prior = 3} will result in using
+#'   \code{(1/4, 1/4, 1/4, 1/4)} as the prior probability for the genotypes
+#'   (Aaaa, AAaa, AAAa, AAAA) where "A" is the reference allele in a 4-ploid
+#'   individual.
+#'
+#' @author David Gerard
+#'
+#' @export
+#'
+bin_post <- function(ncounts, ssize, prior) {
+
+  ## check input -------------------------------------------------------------
+  assertthat::assert_that(all(ncounts >= 0))
+  assertthat::assert_that(all(ssize >= 1))
+  assertthat::assert_that(all(prior >= 0))
+  assertthat::assert_that(all(ssize >= ncounts))
+
+
+  if (sum(prior == 1)) {
+    ploidy <- length(prior) - 1
+  } else {
+    ploidy <- prior
+    prior  <- rep(1 / (ploidy + 1), length = ploidy + 1)
+  }
+
+  assertthat::are_equal(sum(prior), 1)
+
+  ## calculate log-probabilities ---------------------------------------------
+  pk <- seq(0, ploidy) / ploidy ## the possible probabilities
+
+  nsum <- sum(ncounts)
+  tsum <- sum(ssize)
+
+  logprob <- nsum * log(pk) + (tsum - nsum) * log(1 - pk) + log(prior)
+  logprob_b <- logprob
+  ## temp <- stats::dbinom(x = nsum, size = tsum, prob = pk, log = TRUE) + log(prior)
+  ## assertthat::are_equal(exp(logprob) / sum(exp(logprob)), exp(temp) / sum(exp(temp)))
+
+
+  ## Exponentiate and deal with special cases --------------------------------
+  if (nsum == 0) {
+    logprob_b[1] <- log(prior[1])
+    logprob_b[1:ploidy] <- logprob_b[1:ploidy] - max(logprob_b[1:ploidy])
+    postprob <- exp(logprob_b) / sum(exp(logprob_b[1:ploidy]))
+  } else if (nsum == tsum) {
+    logprob_b[ploidy + 1] <- log(prior[ploidy + 1])
+    logprob_b[2:(ploidy + 1)] <- logprob_b[2:(ploidy + 1)] - max(logprob_b[2:(ploidy + 1)])
+    postprob <- exp(logprob_b) / sum(exp(logprob_b[2:(ploidy + 1)]))
+  } else {
+    logprob_b[2:ploidy] <- logprob_b[2:ploidy] - max(logprob_b[2:ploidy])
+    postprob <- exp(logprob_b) / sum(exp(logprob_b[2:ploidy]))
+  }
+
+  ## test when third case only
+  ## assertthat::are_equal(postprob, exp(logprob) / sum(exp(logprob)))
+
+  return(postprob)
+}
---
+++ tests/testthat/test_update.R
@@ -0,0 +1,39 @@
+context("updog")
+
+
+test_that("bin_post works", {
+  set.seed(18)
+  ncounts <- c(11, 13)
+  ssize   <- c(21, 37)
+  prior   <- 6
+
+  postprob <- bin_post(ncounts = ncounts, ssize = ssize, prior = prior)
+  expect_equal(sum(postprob), 1)
+
+  ## test zeros
+  postprob1 <- bin_post(ncounts = 0, ssize = 2, prior = 2)
+  postprob2 <- bin_post(ncounts = 2, ssize = 2, prior = 2)
+  temp <- ((1 - seq(0, 2) / 2) ^ 2) / 3
+  expect_true(all(abs(temp / sum(temp) - postprob1) < 10 ^ -14))
+  expect_true(all(abs(temp / sum(temp) - postprob2[3:1]) < 10 ^ -14))
+}
+)
+
+test_that("updog works", {
+  set.seed(734)
+  ocounts  <- c(11, 13, 0, 11)
+  osize    <- c(21, 37, 2, 11)
+  p1counts <- c(11, 9, 7, 7)
+  p1size   <- c(23, 29, 17, 15)
+  p2counts <- c(7, 9, 11)
+  p2size   <- c(11, 9, 11)
+  ploidy   <- 6
+
+  uout <- updog(ocounts = ocounts, osize = osize, p1counts = p1counts,
+                p1size = p1size, p2counts = p2counts, p2size = p2size,
+                ploidy = ploidy)
+
+  expect_true(all(rowSums(uout) == 1))
+
+}
+)
